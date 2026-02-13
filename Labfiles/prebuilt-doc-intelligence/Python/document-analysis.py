9c85c29da26fd2aee1006eb23e8d6a2cf2ed7c6f
Samuel-Jung69 <samuel.jung@ontario.ca>
2026-02-13 16:38:43 -0500

    Update document-analysis.py for Azure Form Recognizer
    Refactor document analysis script to use Azure SDK and remove dotenv dependency.

--- Labfiles/prebuilt-doc-intelligence/Python/document-analysis.py
+++ Labfiles/prebuilt-doc-intelligence/Python/document-analysis.py
@@ -1,49 +1,40 @@
-from dotenv import load_dotenv
-import os
+from azure.core.credentials import AzureKeyCredential
+from azure.ai.formrecognizer import DocumentAnalysisClient
 
-# Add references
+# Store connection information
+endpoint = "<Endpoint URL>"
+key = "<API Key>"
 
+fileUri = "https://github.com/MicrosoftLearning/mslearn-ai-document-intelligence/blob/main/Labfiles/01-prebuild-models/sample-invoice/sample-invoice.pdf?raw=true"
+fileLocale = "en-US"
+fileModelId = "prebuilt-invoice"
 
+print (f"\nConnecting to Forms Recognizer at: {endpoint}")
+print (f"Analyzing invoice at: {fileUri}")
 
-def main():
+# Create the client
+document_analysis_client = DocumentAnalysisClient(
+    endpoint=endpoint,
+    credential=AzureKeyCredential(key)
+)
 
-    # Clear the console
-    os.system('cls' if os.name=='nt' else 'clear')
+# Analyse the invoice
+poller = document_analysis_client.begin_analyze_document_from_url(
+    fileModelId,
+    fileUri,
+    locale=fileLocale
+)
 
-    try:
-        # Get config settings
-        load_dotenv()
-        endpoint = os.getenv('ENDPOINT')
-        key = os.getenv('KEY')
-
-
-        # Set analysis settings
-        fileUri = "https://github.com/MicrosoftLearning/mslearn-ai-information-extraction/blob/main/Labfiles/prebuilt-doc-intelligence/sample-invoice/sample-invoice.pdf?raw=true"
-        fileLocale = "en-US"
-        fileModelId = "prebuilt-invoice"
-
-        print(f"\nConnecting to Forms Recognizer at: {endpoint}")
-        print(f"Analyzing invoice at: {fileUri}")
-
-
-        # Create the client
-
-
-
-        # Analyse the invoice
-
-
-
-        # Display invoice information to the user
-
-
-            
-
-
-    except Exception as ex:
-        print(ex)
-
-    print("\nAnalysis complete.\n")
-
-if __name__ == "__main__":
-    main()        
+# Display invoice information to the user
+receipts = poller.result()
+for idx, receipt in enumerate(receipts.documents):
+    vendor_name = receipt.fields.get("VendorName")
+    if vendor_name:
+        print (f"\nVendor Name:  {vendor_name.value}, with confidence  {vendor_name.confidence}.")
+    customer_name = receipt.fields.get("CustomerName")
+    if customer_name:
+        print (f"Customer Name: '{customer_name.value}, with confidence  {customer_name.confidence}.")
+    invoice_total = receipt.fields.get("InvoiceTotal")
+    if invoice_total:
+        print (f"Invoice Total: '{invoice_total.value.symbol}{invoice_total.value.amount}, with confidence  {invoice_total.confidence}.")
+print ("\nAnalysis complete.\n")
